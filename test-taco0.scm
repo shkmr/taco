5ea0f134190f00f4d8251aa92f02474fd16f4646
Shigenobu Kimura <skimu@mac.com>
2015-12-21 11:46:06 -0500

    cleanup

--- test-taco0.scm
+++ test-taco0.scm
@@ -6,51 +6,50 @@
 (test-module 'taco0)
 
 (define (taco0-compile-string str)
+  (display "input: ") (write str)
+  (display " => ")
   (let ((ika (with-input-from-string str compile-taco0)))
-    (write str)
-    (display " => ")
     (if ika
         (ika/pp ika)
         (print ika))
     ika))
 
-#|
-   (vm-dump-code (compile '(+ 1.234 a) (interaction-environment)))
-|#
-
+(define taco.out (open-output-file "taco0.out"))
 
 (define (run-test in out)
-  (test* in out (with-input-from-string in compile-taco0)))
+  (test #"\"~|in|\"" out (lambda ()
+                           (with-output-to-port taco.out
+                             (lambda ()
+                               (taco0-compile-string in))))))
 
-(run-test "1\n"
-          '((CONST 1)))
-(run-test "-1\n"
-          '((CONST 1)
-            (NEGATE)))
-(run-test "+1\n" #f)             ; this is error 
-(run-test "1 + -2\n"             ; but this is OK
-          '((CONST 1)
-            (PUSH)
-            (CONST 2)
-            (NEGATE)
-            (NUMADD2))) 
-(run-test "1.234\n" '((CONST 1.234)))
-(run-test " 1 + 2\n"
-          '((CONST 1)
-            (PUSH)
-            (CONST 2)
-            (NUMADD2)))
-(run-test " 1   2\n" #f)
-(run-test "(1 + 2)*(3+4)\n"
-          '((CONST 1)
-            (PUSH)
-            (CONST 2)
-            (NUMADD2)
-            (PUSH)
-            (CONST 3)
-            (PUSH)
-            (CONST 4)
-            (NUMADD2)
-            (NUMMUL2)))
+(run-test  "1\n"      '((CONST 1)))
+(run-test  "-1\n"     '((CONST 1)
+                        (NEGATE)))
+(run-test  "+1\n" #f)             ; this is error 
+(run-test  "1 + -2\n"             ; but this is OK
+                      '((CONST 1)
+                        (PUSH)
+                        (CONST 2)
+                        (NEGATE)
+                        (NUMADD2))) 
+(run-test  "1.234\n"   '((CONST 1.234)))
+(run-test  " 1 + 2\n"  '((CONST 1)
+                         (PUSH)
+                         (CONST 2)
+                         (NUMADD2)))
+(run-test  " 1   2\n" #f)
+(run-test  "(1 + 2)*(3+4)\n"  '((CONST 1)
+                                (PUSH)
+                                (CONST 2)
+                                (NUMADD2)
+                                (PUSH)
+                                (CONST 3)
+                                (PUSH)
+                                (CONST 4)
+                                (NUMADD2)
+                                (NUMMUL2)))
+
+(close-port taco.out)
 
 (test-end :exit-on-failure #t)
+;; EOF
