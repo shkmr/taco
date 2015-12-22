a26d3865efff6fdc4766fadf21da79047e182218
Shigenobu Kimura <skimu@mac.com>
2015-12-22 14:19:16 -0500

    cleanup

--- make-insn-table.scm
+++ make-insn-table.scm
@@ -8,21 +8,50 @@
         (info (cdr name&info)))
     (if (not (eq? name (~ info'name)))
       (error "name does not match: " name (~ info'name)))
-    (print (if (~ info'obsoleted) "<tr bgcolor=aaaaaa>" "<tr>")
+    (print (cond ((~ info'obsoleted) "<tr bgcolor=#aaaaaa>")
+                 ((~ info'combined)  "<tr bgcolor=#aa7777>")
+                 (else "<tr>"))
            "<td><code>" (~ info'name) "</code>"
-           "<td>" (~ info'obsoleted)
-           "<td>" (~ info'num-params)
-           "<td>" (~ info'alt-num-params)
+           "<td align=CENTER>" (~ info'obsoleted)
+           "<td align=CENTER>" (~ info'num-params)
+           "<td align=CENTER>" (~ info'alt-num-params)
            "<td>" (~ info'operand-type)
            "<td>" (if (~ info'combined) "#t" "#f")
            "<td>" (~ info'fold-lref)
-           "<td><pre>" (~ info'body) "</pre>"
            "</tr>")
     ))
 
+(print "<h1>Gauche VM quick rerefernce</h1>")
+
+(print "\
+<p>
+<font style=\"background-color:#aa7777\">This color</font>
+is for combined insns and
+<font style=\"background-color:#aaaaaa\">this color</font>
+is for obsoleted insns.
+</p>
+<p>
+<code>CONST</code> and <code>LREF</code> are
+given special treatment in CompiledCodeBuilder.
+</p>
+See src/code.c src/vminsn.c for details.</p>
+")
+
 (print "<table border=1>")
-(print "<tr><th>&nbsp;&nbsp;name&nbsp;&nbsp;<th>o?<th>p<th>ap<th>ot<th>c<th>fl<th>body</tr>")
+
+(print "\
+<tr><th>name
+<th>obsoleted
+<th>num-params
+<th>alt-num-params
+<th>operand type
+<th>combined
+<th>fold-lref
+</tr>
+")
+
 (for-each insn/pp (reverse (class-slot-ref <vm-insn-info> 'all-insns)))
+
 (print "</table>")
 
 ;; EOF
