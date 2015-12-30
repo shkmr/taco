5aa21dfb9ef3b7191312526a561a4e8a8382b04b
Shigenobu Kimura <skimu@mac.com>
2015-12-30 17:07:28 -0500

    added a few accessor for <compiled-code>

--- vmhack/vmhack.scm
+++ vmhack/vmhack.scm
@@ -1,7 +1,15 @@
 ;;;
 ;;; vmhack
 ;;;
-(define-module vmhack (export vm-code-execute!))
+(define-module vmhack
+  (export vm-code-execute!
+          compiled-code-required-args
+          compiled-code-optional-args
+          compiled-code-args
+          compiled-code-name
+          compiled-code-code-size
+          compiled-code-maxstack
+          ))
 (select-module vmhack)
 (dynamic-load "vmhack")
 ;; EOF
