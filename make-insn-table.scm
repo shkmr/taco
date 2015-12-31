bfd9833fb61a511ae637dbabedefed88a02a304f
Shigenobu Kimura <skimu@mac.com>
2015-12-31 14:31:45 -0500

    added html header and useful documents

--- make-insn-table.scm
+++ make-insn-table.scm
@@ -3,9 +3,13 @@
 ;;;
 (use gauche.vm.insn)
 
-(print "<h1>Gauche VM quick rerefernce</h1>")
-
-(print "\
+(print "<html>
+<head>
+<meta charset=\"utf-8\">
+<title>Gauche VM quick reference</title>
+</head>
+</body>
+<h1>Gauche VM quick rerefernce</h1>
 <p>
 <font style=\"background-color:#aa7777\">This color</font>
 is for combined insns and
@@ -16,11 +20,10 @@ is for obsoleted insns.
 <code>CONST</code> and <code>LREF</code> are
 given special treatment in CompiledCodeBuilder.
 </p>
-See src/code.c src/vminsn.c for details.</p>
+See src/libcode.scm src/code.c src/vminsn.c for details.</p>
 ")
 
 (print "<table border=1>")
-
 (print "\
 <tr><th>name
 <th>obsoleted
@@ -58,4 +61,18 @@ See src/code.c src/vminsn.c for details.</p>
 
 (print "</table>")
 
+(print "
+<h2>Useful domcuments</h2>
+<ul>
+<li> <a href=\"http://practical-scheme.net/docs/stack-j.html\">Schemeの実装におけるスタックフレーム(Draft)</a>
+<li> <a href=\"http://practical-scheme.net/gauche/memo-j.html\">Gauche/memo-j</a>
+<li> <a href=\"http://practical-scheme.net/gauche/memo-stack-j.html\">VMのスタック操作 (未完)</a>
+<li> <a href=\"http://practical-scheme.net/wiliki/wiliki.cgi?Gauche%3AVMの最適化\">VMの最適化</a>
+</ul>
+")
+
+(print "
+</body>
+</html>
+")
 ;; EOF
