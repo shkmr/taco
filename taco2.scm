5c5af132c90659e36e5dc1565258918c1841e578
Shigenobu Kimura <skimu@me.com>
2016-02-17 01:21:23 -0600

    update lang

--- taco2.scm
+++ taco2.scm
@@ -451,15 +451,7 @@
 ;;;  API
 ;;;
 (define make-lalr-token
-  (case (lalr-version)
-    ((v2.4.1 v2.5.0)
-     (lambda (tlex-token)
-       (let ((loc (make-source-location #f #f #f -1 -1)))
-         (if (pair? tlex-token)
-           (make-lexical-token (car tlex-token) loc (cdr tlex-token))
-           (make-lexical-token tlex-token loc tlex-token)))))
-    ((v2.1.0)
-     (lambda  (tlex-token) tlex-token))))
+  (lambda  (tlex-token) tlex-token))
 
 (define (taco2)
   (taco2-parser (lambda () (make-lalr-token (tlex))) error))
--- taco3.scm
+++ taco3.scm
@@ -397,15 +397,7 @@
 (define *show-prompt* #f) ; repl mode if #t
 
 (define make-lalr-token
-  (case (lalr-version)
-    ((v2.4.1 v2.5.0)
-     (lambda (tlex-token)
-       (let ((loc (make-source-location #f #f #f -1 -1)))
-         (if (pair? tlex-token)
-           (make-lexical-token (car tlex-token) loc (cdr tlex-token))
-           (make-lexical-token tlex-token loc tlex-token)))))
-    ((v2.1.0)
-     (lambda  (tlex-token) tlex-token))))
+  (lambda  (tlex-token) tlex-token))
 
 (define (read-taco3)
   (call/cc (lambda (k)
